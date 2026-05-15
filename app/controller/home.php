5325e51433cf8bd3ee7ba07ae529410e50bebed7
tienes <213659684+tienes607@users.noreply.github.com>
2026-05-15 16:42:02 +0700

    URL Process

---
+++ app/controller/home.php
@@ -0,0 +1,8 @@
+<?php
+class Home
+{
+    public function index()
+    {
+        echo "Đây là trang chủ";
+    }
+}
---
+++ app/controller/sinhvien.php
@@ -0,0 +1,12 @@
+<?php
+class SinhVien
+{
+    public function index()
+    {
+        echo "Đây là trang tạo sinh viên";
+    }
+    public function create()
+    {
+        echo "Đây là trang tạo sinh viên";
+    }
+}
