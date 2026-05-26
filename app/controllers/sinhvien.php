802e99c7bcb95bed2a8e8435693307cf55605810
tienes <213659684+tienes607@users.noreply.github.com>
2026-05-26 21:48:36 +0700

    feat: Hiển thị danh sách sinh viên

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,29 +1,13 @@
-<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-class SinhVien
-{
-    public function index()
-    {
-        require_once '../app/views/sinhvien/index.php';
-    }
-    public function create()
-    {
-        require_once '../app/views/sinhvien/create.php';<?php
-class sinhvien {
-function index() {
-        echo "Đây là danh sách sinh viên";
-        //trả về view 
-        require_once '../app/views/sinhvien/index.php';
+<?php
+require_once '../app/core/Controller.php';
+class sinhvien extends Controller {
+    function index() {
+        $SinhvienModel = $this->model('SinhvienModel');
+        $sinhvien = $SinhvienModel -> getAllSinhvien();
+        $this -> view('sinhvien/index', ['sinhvien' => $sinhvien]);
     }
 
     function create() {
-        //trả về view 
         require_once '../app/views/sinhvien/create.php';
-}
-}
-    }
-    public function login()
-    {
-        require_once '../app/views/home/login.php';
     }
 }
-?>
