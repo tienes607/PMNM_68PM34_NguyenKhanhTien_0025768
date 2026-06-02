09be7cee204e7aaa4e3ef38edac1f8cbf4a8f856
tienes <213659684+tienes607@users.noreply.github.com>
2026-06-02 23:16:47 +0700

    feat<sinhvien>: create Sinhvien

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -2,12 +2,27 @@
 require_once '../app/core/Controller.php';
 class sinhvien extends Controller {
     function index() {
-        $SinhvienModel = $this->model('SinhvienModel');
-        $sinhvien = $SinhvienModel -> getAllSinhvien();
-        $this -> view('sinhvien/index', ['sinhvien' => $sinhvien]);
+        $sinhvienModel = $this->model('sinhvienModel');
+        $sinhvien = $sinhvienModel -> getAllSinhvien();
+        $this -> view('layout/masterlayout', ['viewname'=> 'sinhvien/index', 'sinhvien' => $sinhvien, 'title' => 'Danh sách sinh viên']);
     }
 
     function create() {
         require_once '../app/views/sinhvien/create.php';
+    }
+    
+    function store() {
+        if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $hoten = $_POST['hoten'] ?? '';
+            $gioitinh = $_POST['gioitinh'] ?? '';
+            $mssv = $_POST['mssv'] ?? '';
+            $sinhvienModel = $this -> model('sinhvienModel');
+            $result = $sinhvienModel -> create($hoten, $gioitinh, $mssv);
+            if($result) {
+                echo "Thêm sinh viên thành công";
+            } else {
+                echo "Thêm sinh viên thất bại";
+            }
+        }
     }
 }
